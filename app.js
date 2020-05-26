3ffd6c8857dc4986fc8d23942a800aedd5b5ab75
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-05-26 12:38:54 +1000

    get form elements, set event handler and attach to DOM

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ var ProjectInput = /** @class */ (function () {
         this.hostElement = document.getElementById('app');
         var importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
-        this.attach();
-    }
-    ProjectInput.prototype.attach = function () {
+        this.element.id = "user-input";
+        //Input Controls on the form    
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.peopleInputElement = this.element.querySelector('#people');
+        //Add submit event handler ot form
+        this.element.addEventListener('submit', this.submitEventHandler.bind(this));
+        //Attach element
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    }
+    ProjectInput.prototype.submitEventHandler = function (event) {
+        //prevent the default form submission
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
     };
     return ProjectInput;
 }());
-var prjInput = new ProjectInput();
+var projInput = new ProjectInput();
--- app.ts
+++ app.ts
@@ -1,19 +1,38 @@
 class ProjectInput{
-    templateElement: HTMLTemplateElement;
-    hostElement:HTMLDivElement;
-    element:HTMLFormElement;
+    templateElement: HTMLTemplateElement; //template
+    hostElement:HTMLDivElement; //div
+    element:HTMLFormElement; //form
+
+    //Input Controls on the form 
+    titleInputElement:HTMLInputElement;
+    descriptionInputElement:HTMLInputElement;
+    peopleInputElement:HTMLInputElement;
 
     constructor(){
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
         this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
         const importedNode = document.importNode(this.templateElement.content, true);        
-        this.element = importedNode.firstElementChild as HTMLFormElement;
-        this.attach();
-    }
-    private attach()
-    {
+        this.element = importedNode.firstElementChild! as HTMLFormElement;
+        this.element.id="user-input";
+
+        //Input Controls on the form    
+        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+        //Add submit event handler ot form
+        this.element.addEventListener('submit',this.submitEventHandler.bind(this));
+
+        //Attach element
         this.hostElement.insertAdjacentElement('afterbegin',this.element);
     }
+    
+    private submitEventHandler(event:Event)
+    {
+        //prevent the default form submission
+        event.preventDefault();    
+        console.log(this.titleInputElement.value) ;
+    }
 }
-const prjInput = new ProjectInput();
+const projInput = new ProjectInput();
